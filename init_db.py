8fc19b414827f1ff830bd149c50912e32e1fbc07
Phylore <ericpade1987@gmail.com>
2025-04-09 08:27:31 +0200

    gottseidank

--- init_db.py
+++ init_db.py
@@ -1,9 +1,21 @@
+# init_db.py
 from app.app import app
 from database import db
-from models import *  # <-- registriert alle Tabellen sauber
+from models import *  # Registriert alle Tabellen sauber
 from app.setup.setup_modelle_import import import_modelle_wenn_notwendig
+from models.benutzer_db import Benutzer
+
+def benutzer_check():
+    benutzer_liste = Benutzer.query.all()
+    if not benutzer_liste:
+        print("⚠️  Achtung: Keine Benutzer in der Datenbank vorhanden!")
+    else:
+        print(f"✅ {len(benutzer_liste)} Benutzer erfolgreich geladen:")
+        for benutzer in benutzer_liste:
+            print(f" - {benutzer.name}")
 
 with app.app_context():
     db.create_all()
     import_modelle_wenn_notwendig()
+    benutzer_check()
 
