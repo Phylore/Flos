16c46911724c05f9d33fa6d58be83f257a8f5846
Phylore <ericpade1987@gmail.com>
2025-04-09 05:05:33 +0200

    yellow card

--- init_db.py
+++ init_db.py
@@ -1,24 +1,9 @@
 from app.app import app
 from database import db
+from models import *  # <-- registriert alle Tabellen sauber
 from app.setup.setup_modelle_import import import_modelle_wenn_notwendig
-from models.teil_db import Teil
-
-# Importiere explizit alle Models, damit sie sicher registriert werden
-from models import modul_db, teil_db, modell_db, kategorie_db
 
 with app.app_context():
-    print("=== SETUP START ===")
-    print("[DEBUG] Registrierte Tabellen vor create_all():")
-    print(list(db.metadata.tables.keys()))
-
-    print("[SETUP] Erzeuge Tabellen falls nötig...")
     db.create_all()
-
-    print("[DEBUG] Registrierte Tabellen nach create_all():")
-    print(list(db.metadata.tables.keys()))
-
-    print("[SETUP] Prüfe, ob Modelle importiert werden müssen...")
     import_modelle_wenn_notwendig()
-
-    print("=== SETUP ENDE ===")
 
--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,10 @@
+from .kategorie_db import Kategorie
+from .modell_db import Modell
+from .modul_db import Modul
+from .teil_db import Teil
+from .zustand_db import Zustand
+from .benutzer_db import Benutzer
+from .geraet_db import Geraet
+from .historie_db import Historie
 
 
