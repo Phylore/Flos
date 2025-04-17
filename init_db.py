88f1e20b747a8d0e9c04610f0197300b32935631
Phylore <ericpade1987@gmail.com>
2025-04-17 13:23:50 +0200

    Alle-Buttons

--- init_db.py
+++ init_db.py
@@ -1,11 +1,9 @@
 from app.app import app, db
 
 from models.benutzer_db import Benutzer
-from models.zustand_db import Zustand
+from models.zustand_db import Zustand, STANDARD_ZUSTAENDE
 from models.modell_db import Modell
 from models.kategorie_db import Kategorie
-from models.geraet_db import Geraet
-from models.zustand_db import Zustand, STANDARD_ZUSTAENDE
 
 from app.setup.setup_modelle_import import import_modelle_wenn_notwendig
 
@@ -16,7 +14,6 @@ def init_zustaende():
                 db.session.add(Zustand(value=value, kategorie=kategorie))
     db.session.commit()
     print("✅ Zustände initialisiert.")
-
 
 def init_benutzer():
     admin = Benutzer(name="admin", rolle="admin")
@@ -39,24 +36,7 @@ def init_benutzer():
 
     db.session.add_all([admin, eric, alper, moritz, batu, jesse])
     db.session.commit()
-    print("✅ Benutzer 'admin', 'eric', 'batu', "" wurden angelegt.")
-
-
-def beispielgeraete_anlegen():
-    admin = Benutzer.query.filter_by(name="admin").first()
-    modell_l10 = Modell.query.filter_by(name="Dreame L10").first()
-    modell_x40 = Modell.query.filter_by(name="Dreame X40").first()
-    zustand = Zustand.query.filter_by(value="Ja", kategorie="Funktioniert").first()
-
-    if not all([admin, modell_l10, modell_x40, zustand]):
-        print("❌ Fehlende Objekte – kein Gerät angelegt.")
-        return
-
-    g1 = Geraet(qrcode="TEST-L10", modell=modell_l10, zustand=zustand, benutzer=admin)
-    g2 = Geraet(qrcode="TEST-X40", modell=modell_x40, zustand=zustand, benutzer=admin)
-    db.session.add_all([g1, g2])
-    db.session.commit()
-    print("✅ Beispielgeräte wurden angelegt.")
+    print("✅ Benutzer wurden angelegt.")
 
 with app.app_context():
     db.drop_all()
@@ -69,5 +49,5 @@ with app.app_context():
     print("[SETUP] Prüfe, ob Modelle importiert werden müssen...")
     import_modelle_wenn_notwendig()
 
-    beispielgeraete_anlegen()
+    print("✅ Setup abgeschlossen.")
 
