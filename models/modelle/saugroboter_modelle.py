0724418e985cb9bf5eab872a8d9b5f70bd303ee5
Phylore <ericpade1987@gmail.com>
2025-04-22 07:46:13 +0200

    Saugrobotoren angelegt

--- models/modelle/saugroboter_modelle.py
+++ models/modelle/saugroboter_modelle.py
@@ -1,3 +1,7 @@
+
+# ====================
+# Dreame
+# ====================
 saugroboter_modelle = {
     "Dreame L10 Ultra": {
         "hersteller": "Dreame",
@@ -9,7 +13,6 @@ saugroboter_modelle = {
         "tests": ["Funktionstest-Standard2"],
         "ersatzpakete": ["Saugroboter-Ersatzteile-Standard1"]
     },
-
     "Dreame L10s Ultra": {
         "hersteller": "Dreame",
         "kategorie": "Saugroboter",
@@ -20,7 +23,6 @@ saugroboter_modelle = {
         "tests": ["Funktionstest-Standard2"],
         "ersatzpakete": ["Saugroboter-Ersatzteile-Standard1"]
     },
-
     "Dreame X40": {
         "hersteller": "Dreame",
         "kategorie": "Saugroboter",
@@ -29,6 +31,116 @@ saugroboter_modelle = {
             "Roboter": "Saugroboter-Roboter-Standard2"
         },
         "tests": ["Funktionstest-Standard2"]
+    },
+
+# ====================
+# iRobot
+# ====================
+    "iRobot J7+": {
+        "hersteller": "iRobot",
+        "kategorie": "Saugroboter",
+        "module": {
+            "Station": "Saugroboter-Station-Standard1",
+            "Roboter": "Saugroboter-Roboter-Standard1"
+        },
+        "tests": ["Funktionstest-Standard1"]
+    },
+    "iRobot J8+": {
+        "hersteller": "iRobot",
+        "kategorie": "Saugroboter",
+        "module": {
+            "Station": "Saugroboter-Station-Standard1",
+            "Roboter": "Saugroboter-Roboter-Standard1"
+        },
+        "tests": ["Funktionstest-Standard1"]
+    },
+    "iRobot J9+": {
+        "hersteller": "iRobot",
+        "kategorie": "Saugroboter",
+        "module": {
+            "Station": "Saugroboter-Station-Standard2",
+            "Roboter": "Saugroboter-Roboter-Standard2"
+        },
+        "tests": ["Funktionstest-Standard2"]
+    },
+    "iRobot Combo10 Max": {
+        "hersteller": "iRobot",
+        "kategorie": "Saugroboter",
+        "module": {
+            "Station": "Saugroboter-Station-Standard2",
+            "Roboter": "Saugroboter-Roboter-Standard2"
+        },
+        "tests": ["Funktionstest-Standard2"]
+    },
+    "iRobot S9+": {
+        "hersteller": "iRobot",
+        "kategorie": "Saugroboter",
+        "module": {
+            "Station": "Saugroboter-Station-Standard1",
+            "Roboter": "Saugroboter-Roboter-Standard1"
+        },
+        "tests": ["Funktionstest-Standard1"]
+    },
+
+# ====================
+# Ecovacs
+# ====================
+    "Ecovacs N30": {
+        "hersteller": "Ecovacs",
+        "kategorie": "Saugroboter",
+        "module": {
+            "Station": "Saugroboter-Station-Standard1",
+            "Roboter": "Saugroboter-Roboter-Standard1"
+        },
+        "tests": ["Funktionstest-Standard1"]
+    },
+    "Ecovacs Deebot X1 Omni": {
+        "hersteller": "Ecovacs",
+        "kategorie": "Saugroboter",
+        "module": {
+            "Station": "Saugroboter-Station-Standard2",
+            "Roboter": "Saugroboter-Roboter-Standard2"
+        },
+        "tests": ["Funktionstest-Standard2"]
+    },
+    "Ecovacs Omni Pro X30": {
+        "hersteller": "Ecovacs",
+        "kategorie": "Saugroboter",
+        "module": {
+            "Station": "Saugroboter-Station-Standard2",
+            "Roboter": "Saugroboter-Roboter-Standard2"
+        },
+        "tests": ["Funktionstest-Standard2"]
+    },
+
+# ====================
+# Eureka
+# ====================
+    "Eureka J12": {
+        "hersteller": "Eureka",
+        "kategorie": "Saugroboter",
+        "module": {
+            "Station": "Saugroboter-Station-Standard1",
+            "Roboter": "Saugroboter-Roboter-Standard1"
+        },
+        "tests": ["Funktionstest-Standard1"]
+    },
+    "Eureka J15": {
+        "hersteller": "Eureka",
+        "kategorie": "Saugroboter",
+        "module": {
+            "Station": "Saugroboter-Station-Standard1",
+            "Roboter": "Saugroboter-Roboter-Standard1"
+        },
+        "tests": ["Funktionstest-Standard1"]
+    },
+    "Eureka J20": {
+        "hersteller": "Eureka",
+        "kategorie": "Saugroboter",
+        "module": {
+            "Station": "Saugroboter-Station-Standard2",
+            "Roboter": "Saugroboter-Roboter-Standard2"
+        },
+        "tests": ["Funktionstest-Standard2"]
     }
 }
-
